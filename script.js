f77a7f218976746ffa210faeb3c85e95cb92f65d
insiyaa <insiyahhajoori@gmail.com>
2018-05-15 05:36:08 +0530

    Complete times table

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 // Master Circle
-var a = 400, b = 400, radius = 200;
+var a = 400, b = 300, radius = 200;
 c.beginPath();
 c.arc(a, b, radius, 0, Math.PI * 2, false);
 c.stroke();
 
 function drawCircle(xs, ys) {
-    r = 3;
+    r = 1;
     console.log(xs);
     c.beginPath();
     c.arc(xs, ys, r, 0, Math.PI * 2, false);
@@ -21,22 +21,39 @@ function drawCircle(xs, ys) {
     c.fill();
 }
 
-var n = 50;
+var n = 300;
 var arc_len = 2 * Math.PI * radius / n;
 var dtheta = arc_len / radius;
 var x = a - radius;
 var y = b;
 var theta = 0;
 
-for (let i = 0; i <= n; i++) {
+var coorArray = [];
+for (let i = 0; i < n; i++) {
     drawCircle(x,y);
-    console.log("drawn");
+    coorArray.push([x,y]);
+    // console.log("drawn");
+    theta += dtheta;
     x = a - (radius * Math.cos(theta));
-    y = b - (radius * Math.sin(theta));
-    theta += dtheta;     
+    y = b - (radius * Math.sin(theta));        
 }
+ 
+// console.log(coorArray);
+var endp;
+var times = 8;
+var r = Math.random() * 255;
+var g = Math.random() * 255;
+var b = Math.random() * 255;
+c.strokeStyle = 'rgba('+r+','+g+','+b+',1)';
 
-
+for (let i = 0; i < coorArray.length; i++) {
+    c.beginPath();
+    c.moveTo(...coorArray[i]);
+    endp = times * i >= n ? (times * i) % n : times * i;
+    console.log(endp);
+    c.lineTo(...coorArray[endp]);
+    c.stroke();   
+}
 
 
 
